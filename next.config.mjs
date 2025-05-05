75a8d6449421de96fddd32a14239ce849ef7cf07
anuidev8 <angelarrieta34@gmail.com>
2025-05-05 18:43:07 -0500

    se ha modificado dashboard layout

--- next.config.mjs
+++ next.config.mjs
@@ -1,39 +1,36 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     eslint: {
-        ignoreDuringBuilds: true,
+      ignoreDuringBuilds: true,
     },
     images: {
-        unoptimized: true,
+      unoptimized: true,
     },
     experimental: {
-        appDir: true,
+      appDir: true,
+      runtime: 'nodejs',
+      serverActions: {
+        allowedOrigins: ['localhost:3000'],
+      },
     },
     async headers() {
-        return [
+      return [
+        {
+          source: '/api/:path*',
+          headers: [
+            { key: 'Access-Control-Allow-Credentials', value: 'true' },
+            { key: 'Access-Control-Allow-Origin', value: '*' },
+            { key: 'Access-Control-Allow-Methods', value: 'GET,DELETE,PATCH,POST,PUT' },
             {
-                source: '/api/:path*',
-                headers: [
-                    { key: 'Access-Control-Allow-Credentials', value: 'true' },
-                    { key: 'Access-Control-Allow-Origin', value: '*' },
-                    { key: 'Access-Control-Allow-Methods', value: 'GET,DELETE,PATCH,POST,PUT' },
-                    { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version' },
-                ],
+              key: 'Access-Control-Allow-Headers',
+              value:
+                'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
             },
-        ]
+          ],
+        },
+      ]
     },
-    // Configuración del runtime para Node.js
-    serverComponents: {
-        runtime: 'nodejs'
-    },
-    // Desactivar el runtime de edge por defecto
-    experimental: {
-        runtime: 'nodejs',
-        serverActions: {
-            allowedOrigins: ['localhost:3000']
-        }
-    },
-    
-}
-
-export default nextConfig;
+  }
+  
+  export default nextConfig
+  
